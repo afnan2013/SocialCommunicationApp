a36d77b7f8a1904ebad579fd68c8e3f1bdf8c4c5
kucse160235 <afnan1635@cseku.ac.bd>
2021-09-24 01:33:17 +0600

    ReactiveForm Added, Form is converted and Working
    1. ngModel is now won't work
    2. We use programmatacally created Froms now.
    3. Read, Edit, Create and Update is working fine with this ReactiveForm

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { ActivatedRoute, ParamMap } from "@angular/router";
+import { title } from "process";
 import { Post } from "../post.model";
 import { PostsService } from "../posts.service";
 
@@ -10,6 +11,7 @@ import { PostsService } from "../posts.service";
   styleUrls: ["./post-create.component.css"]
 })
 export class PostCreateComponent implements OnInit{
+  form: FormGroup;
   private mode: string = 'create';
   private postId: string;
   post: Post;
@@ -18,19 +20,29 @@ export class PostCreateComponent implements OnInit{
   constructor(public postsService: PostsService, private route: ActivatedRoute){};
 
   ngOnInit(){
+    this.form = new FormGroup({
+      title: new FormControl(null, {validators: [Validators.required, Validators.minLength(5)]}),
+      content: new FormControl(null, {validators: [Validators.required]})
+    });
+
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
         this.mode = "edit";
         this.postId = paramMap.get('postId');
 
         this.postsService.getPost(this.postId).subscribe(responseData => {
+          this.isLoading = false;
           // console.log(responseData.post);
           this.post = {
             id: responseData.post.id,
             title: responseData.post.title,
             content: responseData.post.content
           };
-          this.isLoading = false;
+
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content
+          });
         });
       }else{
         this.mode = 'create';
@@ -39,26 +51,26 @@ export class PostCreateComponent implements OnInit{
     });
   }
 
-  onSavePost(form: NgForm){
-    if(form.invalid){
+  onSavePost(){
+    if(this.form.invalid){
       return;
     }
     this.isLoading = true;
     if (this.mode === 'create'){
       const post: Post = {
         id: null,
-        title: form.value.title,
-        content: form.value.content
+        title: this.form.value.title,
+        content: this.form.value.content
       }
       this.postsService.addPost(post);
     }else{
       const post: Post = {
         id: this.postId,
-        title: form.value.title,
-        content: form.value.content
+        title: this.form.value.title,
+        content: this.form.value.content
       }
       this.postsService.updatePost(post);
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
